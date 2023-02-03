15322580569184c72c435f228508bf85b2b4bf68
fernando301097 <anred123456@gmail.com>
2023-02-03 17:26:49 -0500

    creando filtros en home

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -2,15 +2,16 @@ import React from "react";
 import "./App.css"
 
 import Navbar from "./components/Navbar/Navbar";
+import Home from "./components/Home/Home";
 
 
 const App = () => {
     return (
         <>
         <Navbar/>
+        <Home/>
         
-        {/*<Main/>
-        <Footer/>*/}
+        
         </>
     )
 }
--- front-end/src/components/Home/Home.tsx
+++ front-end/src/components/Home/Home.tsx
@@ -1,100 +1,87 @@
 import React from "react";
-import "./home.css"
-import video from "../../Assets/video.mp4";
-import {GrLocation} from "react-icons/gr"
-import {HiFilter} from "react-icons/hi"
-import {FiFacebook} from "react-icons/fi"
-import {AiOutlineInstagram} from "react-icons/ai"
-import {BsListTask} from "react-icons/bs"
-import {TbApps} from "react-icons/tb"
+
+import "./home.css";
+
+import {GrLocation} from "react-icons/gr";
+import {HiFilter} from "react-icons/hi";
+import {FiFacebook} from "react-icons/fi";
+import {AiOutlineInstagram} from "react-icons/ai";
+import {BsListTask} from "react-icons/bs";
+import {TbApps} from "react-icons/tb";
 
 
-const Home = () => {
-    return (
-        <section className="home">
-            <div className="overlay"></div>
-            <video src={video} muted autoPlay loop type="video/mp4"></video>
+interface HomeProps {}
 
-            <div className="homeContent container">
-                <div className="textDiv">
+const Home: React.FC<HomeProps> = () => {
+  return (
+    <section className="home">
+      <div className="overlay"></div>
+      
+      <div className="homeContent container">
+        <div className="textDiv">
+          <span className="smallText">Ofertas imperdibles</span>
+          <h1 className="homeTitle">ENCUENTRA EL VIAJE DE TUS SUEÑOS</h1>
+        </div>
 
-                    <span className="smallText">
-                        Ofertas imperdibles
-                    </span>
-
-                    <h1 className="homeTitle">
-                        ENCUENTRA EL VIAJE DE TUS SUEÑOS
-                    </h1>
-
-                </div>
-
-                <div className="cardDiv grid">
-
-                    <div className="OriginInput">
-                        <label htmlFor="city">
-                            Origen
-                        </label>
-                        <div className="input flex">
-                            <input type="text" placeholder="Ciudad de origen"/>
-                            <GrLocation className="icon"/>
-                        </div>
-                    </div>
-
-                    <div className="destinationInput">
-                        <label htmlFor="city">
-                            Destino
-                        </label>
-                        <div className="input flex">
-                            <input type="text" placeholder="Ciudad de destino"/>
-                            <GrLocation className="icon"/>
-                        </div>
-                    </div>
-
-                    <div className="dateInputI">
-                        <label htmlFor="date">
-                            Fecha de Ida
-                        </label>
-                        <div className="input flex">
-                            <input type="date"/>
-                        </div>
-                    </div>
-
-                     <div className="dateInputV">
-                        <label htmlFor="date">
-                            Fecha de Vuelta
-                        </label>
-                        <div className="input flex">
-                            <input type="date"/>
-                        </div>
-                    </div>
-
-                    <div className="searchOptions flex">
-                        <HiFilter className="icon"/>
-                        <span> Filtros </span>
-                    </div>
-
-                    
-                    
-                </div>
-
-                <div className="homeFooterIcon flex">
-                    <div className="rightIcons">
-                        <FiFacebook className="icon"/>
-                        <AiOutlineInstagram className="icon"/>
-
-                    </div>
-
-                    <div className="leftIcons">
-                        <BsListTask className="icon"/>
-                        <TbApps className="icon"/>
-
-                    </div>
-
-                </div>
+        <div className="cardDiv grid">
+          <div className="OriginInput">
+            <label htmlFor="city">Origen</label>
+            <div className="input flex">
+              <select placeholder="Ciudad de origen" />
+              <GrLocation className="icon" />
             </div>
+          </div>
 
-        </section>
-    )
-}
+          <div className="destinationInput">
+            <label htmlFor="city">Destino</label>
+            <div className="input flex">
+              <input type="text" placeholder="Ciudad de destino" />
+              <GrLocation className="icon" />
+            </div>
+          </div>
 
-export default Home
+          <div className="dateInputI">
+            <label htmlFor="date">Fecha de Ida</label>
+            <div className="input flex">
+              <input type="date" />
+            </div>
+          </div>
+
+          <div className="dateInputV">
+            <label htmlFor="date">Fecha de Vuelta</label>
+            <div className="input flex">
+              <input type="date" />
+            </div>
+          </div>
+
+          <div className="destinationInput">
+            <label htmlFor="city">Pasajeros</label>
+            <div className="input flex">
+              <input type="text" placeholder="Ciudad de destino" />
+              <GrLocation className="icon" />
+            </div>
+          </div>
+
+          <div className="searchOptions flex">
+            <HiFilter className="icon" />
+            <span> Filtros </span>
+          </div>
+        </div>
+
+        <div className="homeFooterIcon flex">
+          <div className="rightIcons">
+            <FiFacebook className="icon" />
+            <AiOutlineInstagram className="icon" />
+          </div>
+
+          <div className="leftIcons">
+            <BsListTask className="icon" />
+            <TbApps className="icon" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
