3a08036b8bc3a7b22a8d7a594a71e2afd92b3fe5
fernando301097 <anred123456@gmail.com>
2023-02-06 19:09:02 -0500

    fixed bug:hammer:

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router } from "react-router-dom";
-import Routing from "./components/Routing";
+import Routing from "./components/routing";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home"
 
